24451acdae7aca286d81eb5a4d4745475c345ea6
Armando-Vidal <armandoamvidal@gmail.com>
2024-03-13 13:52:05 -0300

    Adiciona entidades e Enums

--- src/main/java/com/armando/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/armando/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,4 +1,4 @@
-package com.armando.pontointeligente;
+package com.armando.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
