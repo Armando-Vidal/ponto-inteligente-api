e3762261d78691b4ea47bd7ed9a5ac4e8e938111
Armando-Vidal <armandoamvidal@gmail.com>
2024-03-13 15:16:37 -0300

    Adiciona testes (já realizados com sucesso)

---
+++ src/test/java/com/armando/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.armando.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+/*import org.springframework.test.context.junit4.SpringRunner;*/
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PontoInteligenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
