9e8fc5a61d8f9ad7bda1ec7e216ac60ba7465f22
Armando-Vidal <armandoamvidal@gmail.com>
2024-03-18 11:16:35 -0300

    Conserta Securityconfig.java com permissões de acordo com https://www.baeldung.com/java-spring-fix-403-error#:~:text=This%20typically%20implies%20that%20the,t%20received%20the%20valid%20credentials

--- src/main/java/com/armando/pontointeligente/api/config/SecurityConfig.java
+++ src/main/java/com/armando/pontointeligente/api/config/SecurityConfig.java
@@ -3,21 +3,43 @@ package com.armando.pontointeligente.api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+          .password(encoder().encode("userPass"))
+          .roles("USER")
+          .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
             )
-            .httpBasic(withDefaults());
+            .httpBasic(withDefaults())
+            .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
