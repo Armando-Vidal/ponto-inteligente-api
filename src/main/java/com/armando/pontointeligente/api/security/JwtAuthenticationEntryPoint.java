a97278dad2b021d67b203fefd5c31c28026d08b1
Armando-Vidal <armandoamvidal@gmail.com>
2024-03-19 14:57:36 -0300

    Adiciona configurações de Token (problema no filtro)

---
+++ src/main/java/com/armando/pontointeligente/api/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.armando.pontointeligente.api.security;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+				"Acesso negado. Você deve estar autenticado no sistema para acessar a URL solicitada.");
+	}
+
+}
